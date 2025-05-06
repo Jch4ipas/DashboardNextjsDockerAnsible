7790087b95c312cac43c4ad0ad4e2bdc5bbbdab0
Jch4ipas <jeremy.wuersch@gmail.com>
2025-05-06 12:30:04 +0200

    [fix] update the clock component

--- nextjs/src/components/Clock.js
+++ nextjs/src/components/Clock.js
@@ -3,10 +3,14 @@ import { useState, useEffect } from "react";
 
 export default function Clock() {
   const [dateTime, setDateTime] = useState(new Date());
+  const [date, setDate] = useState(new Date().toLocaleDateString());
+  const [hour, setHour] = useState(new Date().toLocaleTimeString());
 
   useEffect(() => {
     const interval = setInterval(() => {
       setDateTime(new Date());
+      setDate(new Date().toLocaleDateString())
+      setHour(new Date().toLocaleTimeString())
     }, 1000);
 
     return () => clearInterval(interval);
@@ -14,8 +18,8 @@ export default function Clock() {
 
   return (
     <div className="flex flex-col items-center justify-center text-xl font-bold">
-      <span>{dateTime.toLocaleDateString()}</span>
-      <span>{dateTime.toLocaleTimeString()}</span>
+      <span>{date}</span>
+      <span>{hour}</span>
     </div>
   );
 }
