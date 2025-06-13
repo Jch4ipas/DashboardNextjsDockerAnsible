bd4093e0580b3a0ecbb46b8d8f915842ca561c64
Jch4ipas <jeremy.wuersch@gmail.com>
2025-06-13 15:26:17 +0200

    [feature] add a cache for APOD media

--- nextjs/src/services/infonasa.jsx
+++ nextjs/src/services/infonasa.jsx
@@ -7,6 +7,8 @@ export default function NasaMedia() {
   const apiKey = process.env.NEXT_PUBLIC_NASA_API_KEY;
   console.log("L'api est : " + apiKey);
   const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
+  const CACHE_KEY = "nasa_apod_cache";
+  const TTL = 1000 * 60 * 60 * 3; // 3heure
 
   if (!apiKey) {
     console.error("API key is missing");
@@ -14,6 +16,20 @@ export default function NasaMedia() {
   }
 
   useEffect(() => {
+    const cachedData = localStorage.getItem(CACHE_KEY);
+
+    if (cachedData) {
+      const parsed = JSON.parse(cachedData);
+      const isExpired = Date.now() > parsed.expiry;
+
+      if (!isExpired) {
+        setMediaData(parsed.data);
+        return;
+      } else {
+        localStorage.removeItem(CACHE_KEY);
+      }
+    }
+
     async function fetchData() {
       try {
         const response = await fetch(apiUrl);
@@ -21,14 +37,26 @@ export default function NasaMedia() {
 
         const data = await response.json();
         setMediaData(data);
+
+        // Mise en cache avec expiration
+        localStorage.setItem(
+          CACHE_KEY,
+          JSON.stringify({
+            data,
+            expiry: Date.now() + TTL,
+          })
+        );
       } catch (error) {
         console.error("Fetch error:", error);
       }
     }
 
     fetchData();
-  }, []);
+  }, [apiUrl]);
 
+  if (!apiKey) {
+    return <p>Erreur : clé API manquante</p>;
+  }
   if (!mediaData) return <p>Chargement...</p>;
 
   return (
@@ -45,7 +73,7 @@ export default function NasaMedia() {
         ></iframe>
       ) : (
         <a href={mediaData.hdurl} target="_blank" rel="noopener noreferrer">
-          <img src={mediaData.url} alt={mediaData.title} width="300" />
+          <img src={mediaData.url} alt={mediaData.title} width="800" />
         </a>
       )}
     </div>
