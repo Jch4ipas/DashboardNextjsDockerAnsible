4fe54bfb918632b8c95503c8e339eea0d25e0132
Jch4ipas <jeremy.wuersch@gmail.com>
2025-06-13 15:27:01 +0200

    [feature] make date and time format to be fr-CH

--- nextjs/src/components/Clock.js
+++ nextjs/src/components/Clock.js
@@ -3,14 +3,14 @@ import { useState, useEffect } from "react";
 
 export default function Clock() {
   const [dateTime, setDateTime] = useState(new Date());
-  const [date, setDate] = useState(new Date().toLocaleDateString());
-  const [hour, setHour] = useState(new Date().toLocaleTimeString());
+  const [date, setDate] = useState(new Date().toLocaleDateString('fr-CH'));
+  const [hour, setHour] = useState(new Date().toLocaleTimeString('fr-CH'));
 
   useEffect(() => {
     const interval = setInterval(() => {
       setDateTime(new Date());
-      setDate(new Date().toLocaleDateString())
-      setHour(new Date().toLocaleTimeString())
+      setDate(new Date().toLocaleDateString('fr-CH'))
+      setHour(new Date().toLocaleTimeString('fr-CH'))
     }, 1000);
 
     return () => clearInterval(interval);
